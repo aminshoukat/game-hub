7353eaa7ce249fd0bbce67d1ffa914fb3b4f253c
Amin Shoukat <aminshoukat4@gmail.com>
2023-12-13 14:34:33 +0500

    Build platform selector

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,9 +9,10 @@ interface Props {
 
 const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
 
-    const {data, isLoading} = useGenres();
+    const { data, isLoading, error } = useGenres();
 
-    if(isLoading) return <Spinner />
+    if(error) return null;
+    if(isLoading) return <Spinner />;
 
     return (
         <>
